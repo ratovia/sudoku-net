b99ef7cf812e199667cce43f05edc85498f524de
ratovia <ratovia@hotmail.co.jp>
2019-06-07 13:21:29 +0900

    set database

--- db/migrate/20190607031244_create_puzzles.rb
+++ db/migrate/20190607031244_create_puzzles.rb
@@ -1,7 +1,9 @@
 class CreatePuzzles < ActiveRecord::Migration[5.2]
   def change
     create_table :puzzles do |t|
-
+      t.references :room,null: false,foreign_key: true
+      t.string :answer,null: false
+      t.string :question,null: false
       t.timestamps
     end
   end
