be865e900adcd3b3f869fddb02c852c847c85fd3
ratovia <ratovia@hotmail.co.jp>
2019-06-19 17:57:31 +0900

    update sudoku script

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,27 +1,34 @@
 $(function(){
   function create_table(){
+    var data = $(".sudoku-table").data("question").split("")
     let parent = $(".sudoku-table tbody");
-    var html = ""
-    var data = ""
+    var html = "";
     for(var i = 0; i < 9; i++){
       if(i == 2 || i == 5){
-        html += `<tr class="bottom-border">`
+        html += `<tr class="bottom-border">`;
       }else{
-        html += `<tr>`
+        html += `<tr>`;
       }
       for(var j = 0; j < 9;j++){
-        if(j == 2 || j == 5){
-          html += `<td class="right-border"><input class="sudoku-cell" id="c${i}_${j}" value='${data}' maxlength="1" pattern="^[1-9]{1}$"></td>`
+        if(data[i*9+j] !== "0"){
+          if(j == 2 || j == 5){
+            html += `<td class="right-border"><input type="number" class="sudoku-cell grayout" id="c${i}_${j}" value='${data[i*9+j]}' disabled maxlength="1" pattern="^[1-9]{1}$"></td>`;
+          }else{
+            html += `<td><input type="number" class="sudoku-cell grayout" id="c${i}_${j}" value='${data[i*9+j]}' disabled maxlength="1" ></td>`;
+          }
         }else{
-          html += `<td><input class="sudoku-cell" id="c${i}_${j}" value='${data}' maxlength="1" ></td>`
+          if(j == 2 || j == 5){
+            html += `<td class="right-border"><input type="number" class="sudoku-cell" id="c${i}_${j}" maxlength="1" pattern="^[1-9]{1}$"></td>`;
+          }else{
+            html += `<td><input type="number" class="sudoku-cell" id="c${i}_${j}" maxlength="1" ></td>`;
+          }
         }
       }
-      html += `</tr>`
+      html += `</tr>`;
 
     }
     parent.append(html);
   }
-
   create_table();
-  reloadData();
+
 });
